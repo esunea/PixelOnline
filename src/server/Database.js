feca3556337cdc924020c40d08e8aae502bca19d
Léo DESIGAUX <leodesigaux@gmail.com>
2018-09-04 21:38:33 +0200

    Setting up a no db database

---
+++ src/server/Database.js
@@ -0,0 +1,41 @@
+export class Database {
+  constructor() {
+    this.USE_DB = false;
+    this.users = [
+      {
+        username:"Leoche",
+        email:"leodesigaux@gmail.com",
+        password:"1234"
+      }
+    ]
+  }
+  login (email, password) {
+    if (!this.USE_DB) {
+      let isLogged = false;
+      this.users.forEach(user => {
+        if (user.email === email && user.password === password) isLogged = true;
+      })
+      return isLogged;
+    }
+  }
+  register (username, email, password) {
+    if (!this.USE_DB) {
+      if (this.isUnique('users', 'username', username) && this.isUnique('users', 'email', email)) {
+        this.users.push({
+          'username': username,
+          'email': email,
+          'password': password
+        })
+      }
+    }
+  }
+  isUnique (collection, field, value) {
+    if (!this.USE_DB) {
+      let isUnique = true;
+      this[collection].forEach(item => {
+        if (item[field] === value) isUnique = false;
+      })
+      return isUnique;
+    }
+  }
+}
