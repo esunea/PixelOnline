564d5d365c8d7e6bd8aa830de1bee469890ebd50
Léo DESIGAUX <leodesigaux@gmail.com>
2018-09-08 23:39:38 +0200

    Notification animation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import {Game} from './Game';
+import $ from 'webpack-zepto'
 import css from './css/style.css'
 var io = require('socket.io-client');
 window.io = io;
 
 window.game = new Game(io("http://localhost:8080"));
+setTimeout(() => $('.appear').toggleClass('appear'), 2000)
+setTimeout(() => $('.notification').toggleClass('appear'), 4000)
